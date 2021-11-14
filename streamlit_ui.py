56c81ad7df56e0105394adb33f77c02eb5101f3d
Brianna Turgott <37252935+briturgott@users.noreply.github.com>
2021-11-14 09:31:50 -0500

    Update streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import json
 import streamlit as st
-
+import main
+main.main();
 st.title("Welcome to Sage Social!")
 
 st.write("Here is your Reddit profile. You can see what posts you have recently seen, and their positivity score.")
@@ -9,6 +10,7 @@ st.write("Here is your Reddit profile. You can see what posts you have recently 
 post_list = open('worldnews_subreddit.json')
 
 if st.button('Update'):
+    main.main();
     st.error('Updated! Make sure to refresh.')
 
 data = json.load(post_list)
@@ -18,9 +20,8 @@ for post in data:
     curated_post = {}
     curated_post["Title"] = post["title"]
     curated_post["Subreddit"] = post["subreddit"]
-    curated_post["Upvotes"] = post["score"]
     curated_post["Username"] = post["author"]
-    #new_post["Positivity"] = post["compound"] #this is where we add the scores
+    curated_post["Positivity"] = post["compound"] #this is where we add the scores
     curated_post_list.append(curated_post)
 
 pretty_data = pd.DataFrame(curated_post_list)
